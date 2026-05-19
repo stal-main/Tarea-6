24965e23f3bb809f1ddbcded78238c868c141205
Kevin Solano <7stalyakov7@gmail.com>
2026-05-19 00:30:15 -0600

    Implementacion basica de radixSort

--- Tarea6.cpp
+++ Tarea6.cpp
@@ -1,7 +1,60 @@
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#include"LinkedList.h"    
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+void radixSort(LinkedList<int>& list, int maxDigits) {
+
+    LinkedList<int> buckets[10];
+
+    int divisor = 1;
+
+    for (int d = 0; d < maxDigits; d++) {
+
+        while (!list.atEnd()) {
+
+            int digit = (list.getElement() / divisor) % 10;
+
+            buckets[digit].append(list.getElement());
+
+            list.next();
+        }
+
+        list.clear();
+
+        for (int i = 0; i < 10; i++) {
+
+            while (!buckets[i].atEnd()) {
+
+                list.append(buckets[i].getElement());
+
+                buckets[i].next();
+            }
+
+            buckets[i].clear();
+        }
+
+        divisor *= 10;
+    }
+}
+
+int main() {
+
+    int listSize;
+
+    int numericBase;
+
+    cout << "Introduce the size of the list to be sorted: " << endl;
+
+	cin >> listSize;
+
+    cout << "Introduce the numeric base to be used: " << endl;
+
+	cin >> numericBase;
+
+
 }
