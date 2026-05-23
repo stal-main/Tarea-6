57d96b1355cb5205fce64c27506f1064889f6bbf
Kevin Solano <7stalyakov7@gmail.com>
2026-05-22 23:49:56 -0600

    Creo que funciona

--- Tarea6.cpp
+++ Tarea6.cpp
@@ -1,32 +1,69 @@
 
 #include <iostream>
-
+#include <cstdlib>
 #include"LinkedList.h"    
 
 using std::cout;
 using std::endl;
 using std::cin;
 
-void radixSort(LinkedList<int>& list, int maxDigits) {
+int getMax(LinkedList<int>& list) {
+    int max = 0;
 
-    LinkedList<int> buckets[10];
+    list.goToStart();
+
+    while (!list.atEnd()) {
+
+        if (list.getElement() > max) {
+
+            max = list.getElement();
+        }
+
+        list.next();
+    }
+
+    return max;
+}
+
+void radixSort(LinkedList<int>& list, int base) {
+
+    if (list.getSize() == 0) {
+        return;
+	}
+
+	int max = getMax(list);
+
+    int numPass = 1;
+
+	int tempMax = max;
+
+    while (tempMax >= base) {
+
+        tempMax /= base;
+
+        numPass++;
+	}
+
+    LinkedList<int>* buckets = new LinkedList<int>[base];
 
     int divisor = 1;
 
-    for (int d = 0; d < maxDigits; d++) {
+    for (int d = 0; d < numPass; d++) {
+
+        list.goToStart();
 
         while (!list.atEnd()) {
 
-            int digit = (list.getElement() / divisor) % 10;
+            int digit = (list.getElement() / divisor) % base;
 
             buckets[digit].append(list.getElement());
 
             list.next();
         }
 
-        list.clear();
+        for (int i = 0; i < base; i++) {
 
-        for (int i = 0; i < 10; i++) {
+			buckets[i].goToStart();
 
             while (!buckets[i].atEnd()) {
 
@@ -38,23 +75,76 @@ void radixSort(LinkedList<int>& list, int maxDigits) {
             buckets[i].clear();
         }
 
-        divisor *= 10;
+        divisor *= base;
     }
+
+    delete[] buckets;
 }
 
 int main() {
 
-    int listSize;
+    srand(time(nullptr));
 
-    int numericBase;
+    bool continuate = true;
 
-    cout << "Introduce the size of the list to be sorted: " << endl;
+    while (continuate) {
 
-	cin >> listSize;
+        int listSize;
 
-    cout << "Introduce the numeric base to be used: " << endl;
+        int numericBase;
 
-	cin >> numericBase;
+        cout << "Introduce the size of the list to be sorted: " << endl;
 
+        cin >> listSize;
+
+        cout << "Introduce the numeric base to be used: " << endl;
+
+        cin >> numericBase;
+
+        if (numericBase < 2) {
+
+            cout << "Numeric base must be at least 2. Try again" << endl;
+		}
+
+        else {
+
+            LinkedList<int> list;
+
+            for (int i = 0; i < listSize; i++) {
+
+                list.append(rand() % 100000);
+            }
+
+            if (listSize <= 20) {
+
+                cout << "Original list: ";
+
+                list.print();
+			}
+
+            clock_t start = clock();
+
+			radixSort(list, numericBase);
+
+            clock_t end = clock();
+
+			double seconds = double(end - start) / CLOCKS_PER_SEC;
+
+            if (listSize <= 20) {
+
+                cout << "Sorted list: ";
+
+                list.print();
+
+            }
+
+			cout << "Sorted " << listSize << " elements in " << seconds << " seconds." << endl;
+
+        }
+
+		cout << "Do you want to sort another list? (y/n)" << endl;
+    }
+
+    return 0;
 
 }
